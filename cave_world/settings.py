3063cef2d9bf4c3fc5351e1c738d165865cac796
Brakanier <18brakanier.forest@gmail.com>
2019-01-16 21:06:56 +0300

    allow hosts

--- cave_world/settings.py
+++ cave_world/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://caveworld.herokuapp.com/']
+ALLOWED_HOSTS = ['caveworld.herokuapp.com']
 
 
 # Application definition
