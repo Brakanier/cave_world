72543611bf0e01ef1511aa75ddcdb006b1b228f7
Brakanier <18brakanier.forest@gmail.com>
2019-01-18 16:16:12 +0300

    Fixes

--- digger/views.py
+++ digger/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 
 from decouple import config
 import json, vk_api
+import random
 
 # Create your views here.
 
@@ -11,23 +12,32 @@ secret_token = config('SECRET_TOKEN')
 confirmation_token = config('CONFIRMATION_TOKEN')
 token = config('TOKEN')
 
+def get_random_id():
+    """ Get random int32 number (signed) """
+    return random.getrandbits(31) * random.choice([-1, 1])
+
 @csrf_exempt
 def index(request):
     if (request.method == 'POST'):
-        data = json.loads(request.body)
+        data = json.loads(request.body.decode("utf-8"))
         if (data['secret'] == secret_token):
             if (data['type'] == 'confirmation'):
                 return HttpResponse(confirmation_token, content_type="text/plain", status=200)
             if (data['type'] == 'message_new'):
-                vk_session = vk_api.VkApi(token=token,api_version=5.90)
+                vk_session = vk_api.VkApi(token=token)
+                vk = vk_session.get_api()
+                '''
                 try:
                     vk_session.auth(token_only=True)
                 except vk_api.AuthError as error_msg:
                     print(error_msg)
-                    return
-                vk = vk_api.get_api()
+                    return HttpResponse(error_msg , content_type="text/plain", status=200)
+                '''
                 user_id = data['object']['from_id']
-                vk.masseges.send(access_token=token, user_id=str(user_id), message='Ответ бота')
+                vk.messages.send(access_token=token, user_id=str(user_id), message='Ответ бота', random_id = get_random_id())
                 return HttpResponse('ok', content_type="text/plain", status=200)
+            return HttpResponse('Ошибка - неверный type')
+        else:
+            return HttpResponse('Ошибка - неверный secret key')
     else:
         return HttpResponse('Сайт находится в разработке!')
