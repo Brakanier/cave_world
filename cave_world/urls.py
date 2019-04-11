99aeab4d9dbe6c8a8993f5f7b5904700aa0edd97
Brakanier <18brakanier.forest@gmail.com>
2019-04-11 16:02:04 +0300

    new migration

--- cave_world/urls.py
+++ cave_world/urls.py
@@ -18,6 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bot/', include('digger.urls')),
-    path('game/', include('game.urls')),
+    path('bot/', include('game.urls')),
 ]
--- digger/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DiggerConfig(AppConfig):
-    name = 'digger'
